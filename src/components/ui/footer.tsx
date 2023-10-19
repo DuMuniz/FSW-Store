71c20cc7e786dd075f8ac167ea1af4c0109f8e19
DuMuniz <eduardo.muniz@maitha.com.br>
2023-10-18 22:21:50 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="text=[0.625rem] bg-accent px-8 py-4 opacity-75">
+      2023 Copyright <span className="font-semibold">Muniz Store</span>
+    </div>
+  );
+};
+
+export default Footer;
