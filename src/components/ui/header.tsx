f1e85f589e41483fa865350661e99aff5a8d5ab0
DuMuniz <eduardo.muniz@maitha.com.br>
2023-10-20 22:10:13 -0300

    feat: ensure user goes to home page on logo click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -108,9 +108,12 @@ const Header = () => {
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">Muniz</span> Store
-      </h1>
+      <Link href={"/"}>
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">Muniz</span> Store
+        </h1>
+      </Link>
+
       <Button size={"icon"} variant={"outline"}>
         <ShoppingCartIcon />
       </Button>
